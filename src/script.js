820b751bc3c724c3bcd432b4e99f29084432ad99
Yann Kamche <yannkamche@gmail.com>
2023-08-29 13:29:03 +0100

    Added scroll to top, background on scroll, set the navbar to fixed and adjusted the body's width

--- src/script.js
+++ src/script.js
@@ -17,3 +17,23 @@ xmarkBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('hidden');
     xmarkBtn.classList.toggle('hidden');
 })
+
+/*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById("scroll-up");
+
+    this.scrollY >= 350 
+    ? scrollUp.classList.add("show-scroll") 
+    : scrollUp.classList.remove("show-scroll");
+}
+
+/*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+
+const scrollHeader = () => {
+    const navBar = document.getElementById("navbar");
+
+    this.scrollY >= 30 
+    ? navBar.classList.add("bg-header") 
+    : navBar.classList.remove("bg-header"); 
+}
+window.addEventListener("scroll", scrollHeader);
